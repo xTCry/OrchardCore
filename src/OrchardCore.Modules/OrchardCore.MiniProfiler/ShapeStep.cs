5892dcd7a90195da38048fb43382d985c6099d47
Kavli-Kaffel <63543200+Kavli-Kaffel@users.noreply.github.com>
2020-05-29 04:08:23 +0300

    Update ShapeStep.cs

--- src/OrchardCore.Modules/OrchardCore.MiniProfiler/ShapeStep.cs
+++ src/OrchardCore.Modules/OrchardCore.MiniProfiler/ShapeStep.cs
@@ -12,6 +12,7 @@ namespace OrchardCore.MiniProfiler
 
         public Task DisplayedAsync(ShapeDisplayContext context)
         {
+        //removing value and disposition of new value
             if (_timings.TryGetValue(context, out IDisposable timing))
             {
                 _timings.Remove(context);
@@ -23,11 +24,13 @@ namespace OrchardCore.MiniProfiler
 
         public Task DisplayingAsync(ShapeDisplayContext context)
         {
+        // adding of timings value
             var timing = StackExchange.Profiling.MiniProfiler.Current.Step($"Shape: {context.Shape.Metadata.Type}");
             _timings.Add(context, timing);
             return Task.CompletedTask;
         }
 
+        // when analysis has ended  
         public Task DisplayingFinalizedAsync(ShapeDisplayContext context)
         {
             if (_timings.TryGetValue(context, out IDisposable timing))
