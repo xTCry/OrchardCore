04c05e4df56a42e3e3db7f62c496c34261114d5b
Kavli-Kaffel <63543200+Kavli-Kaffel@users.noreply.github.com>
2020-06-04 04:39:13 +0300

    Update IThemeService.cs

--- src/OrchardCore.Modules/OrchardCore.Themes/Services/IThemeService.cs
+++ src/OrchardCore.Modules/OrchardCore.Themes/Services/IThemeService.cs
@@ -4,6 +4,7 @@ namespace OrchardCore.Themes.Services
 {
     public interface IThemeService
     {
+        //allows you to turn on and off themes
         Task DisableThemeFeaturesAsync(string themeName);
         Task EnableThemeFeaturesAsync(string themeName);
     }
